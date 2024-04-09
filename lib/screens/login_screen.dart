a97cf8be53e6eca329a9ba5164e84cf9b4adfa41
IGSparkew <alex.robbrecht@sfr.fr>
2024-04-09 17:09:22 +0200

    add authentification

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+
+class LoginScreen extends StatelessWidget {
+  LoginScreen({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
