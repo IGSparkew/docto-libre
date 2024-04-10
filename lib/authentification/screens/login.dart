ae41b4b9e9695b23224105828db66fa483ade0e2
IGSparkew <alex.robbrecht@sfr.fr>
2024-04-10 10:43:32 +0200

    add named routes and structure

--- lib/authentification/screens/login.dart
+++ lib/authentification/screens/login.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
-
-
 class LoginScreen extends StatelessWidget {
   LoginScreen({super.key});
 
-
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+        return Scaffold(
+          body: Center(
+            child: Text('Hello World!'),
+          ),
+        );
   }
 
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:docto_libre/appointement/screens/appointement.dart';
 import 'package:flutter/material.dart';
+
+import 'appointement/screens/appointement_list.dart';
+import 'authentification/screens/login.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,14 +13,15 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Docto Libre',
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => LoginScreen(),
+        '/appointements': (context) => AppointementListScreen(),
+        '/appointement': (context) => AppointementScreen(),
+      },
     );
   }
 }
