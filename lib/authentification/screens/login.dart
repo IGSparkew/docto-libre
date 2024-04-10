48cfa29851a3d9c013c3a92836c20f51285cf8c0
IGSparkew <alex.robbrecht@sfr.fr>
2024-04-10 13:55:46 +0200

    add header bar

--- lib/authentification/screens/login.dart
+++ lib/authentification/screens/login.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 
+import '../../componements/header.dart';
+
 class LoginScreen extends StatelessWidget {
   LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
         return Scaffold(
+          appBar: Header(),
           body: Center(
-            child: Text('Hello World!'),
+            child: Text('Login'),
           ),
         );
   }
