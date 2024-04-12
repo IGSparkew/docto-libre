63ff67ee14c9b63077f6cc5e7a4d830e83ffc53a
IGSparkew <alex.robbrecht@sfr.fr>
2024-04-12 10:30:41 +0200

    add login erro componement and make repo more useful

--- lib/authentification/screens/login.dart
+++ lib/authentification/screens/login.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import '../../componements/header.dart';
+import '../components/loginfailedcomponement.dart';
 
 class LoginScreen extends StatefulWidget {
   LoginScreen({super.key});
@@ -112,30 +113,5 @@ class LoginScreenState extends State<LoginScreen> {
                 ],
               )),
         )));
-  }
-}
-
-class LoginFailedComponement extends StatelessWidget {
-  LoginFailedComponement({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-        padding: EdgeInsets.only(bottom: 32),
-        child: Container(
-          width: 400,
-          height: 100,
-          decoration: BoxDecoration(
-            color: Color.fromRGBO(255, 51, 51, 0.05),
-            borderRadius: BorderRadius.circular(10),
-            border: Border.all(color: Colors.red, width: 2),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Identifiant ou mot de passe incorrect',style: TextStyle(color: Color.fromRGBO(255, 51, 51, 1), decorationStyle: TextDecorationStyle.solid))
-            ]
-          ),
-        ));
   }
 }
